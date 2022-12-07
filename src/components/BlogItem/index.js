a3c4f1a0019020366f7a66b0671ab8d395cceef5
belladi-sharath-chandra <bsharathchandra9@gmail.com>
2022-12-07 11:03:53 +0000

    blog list

---
+++ src/components/BlogItem/index.js
@@ -0,0 +1,19 @@
+// Write your JS code here
+import './index.css'
+
+const BlogItem = props => {
+  const {blogDetails} = props
+  const {title, description, publishedDate} = blogDetails
+
+  return (
+    <li className="list">
+      <div className="title">
+        <h1 className="post">{title}</h1>
+        <p className="date">{publishedDate}</p>
+      </div>
+      <p className="description">{description}</p>
+    </li>
+  )
+}
+
+export default BlogItem
---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,16 @@
+// Write your JS code here
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-info">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      alt="profile"
+      className="img"
+    />
+    <h1 className="name">Wade Warren"</h1>
+    <p className="role">Software Developer at uk</p>
+  </div>
+)
+
+export default UserInfo
